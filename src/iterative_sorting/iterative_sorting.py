0e009970d6521e81fa7d7a27fbc4cd9a0ebc387c
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-18 15:42:28 -0500

    finished iterative_sorting.py

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,10 +4,10 @@ def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-    for smallest_index in range(cur_index + 1, len(arr)):
-        if arr[smallest_index] < arr[cur_index]:
+        for smallest_index in range(cur_index + 1, len(arr)):
+            if arr[smallest_index] < arr[cur_index]:
         # TO-DO: swap
-            arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+                arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
 
