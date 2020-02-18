66fc2f240a12fd9bb252ceaf365688027b57ae61
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-02-18 15:37:46 -0500

    still trying to debug selection_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,5 +1,5 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
@@ -11,11 +11,13 @@ def selection_sort( arr ):
 
     return arr
 
-
-
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-
+def bubble_sort(arr):
+    for i in range(len(arr)-1, 0, -1):
+        for sort in range(0, i):
+           if arr[sort] > arr[sort+1]:
+                # swap
+                arr[sort], arr[sort+1] = arr[sort+1], arr[sort] 
     return arr
 
 
